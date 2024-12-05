c74a8cd5b7923a56f43c72b7260ead255453cb59
曦暮流年 <2515550574@qq.com>
2024-12-05 21:58:43 +0800

    BUG修复

--- src/main.js
+++ src/main.js
@@ -6,14 +6,11 @@ import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
 
 app.provide("config", config)
 
