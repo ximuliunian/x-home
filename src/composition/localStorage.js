e8f4049fda1ab633cb3527661548fd7b70ab455f
曦暮流年 <2515550574@qq.com>
2025-02-28 11:18:58 +0800

    - 添加 gossip 相关的命名空间，并自定义初始化内容

--- src/composition/localStorage.js
+++ src/composition/localStorage.js
@@ -4,7 +4,25 @@ const NAMESPACE = 'xmln_home'
 
 // 空间命名枚举
 const ModelMenu = {
-    HITOKOTO: 'hitokoto'
+    HITOKOTO: 'hitokoto',
+    GOSSIP: 'gossip',
+    GOSSIP_SETTING: 'gossip_setting',
+}
+
+// 空间命名初始化
+const initContent = {
+    [ModelMenu.GOSSIP]: {
+        // 友人链接
+        links: [],
+        // 公开信息
+        publicGossip: []
+    },
+    [ModelMenu.GOSSIP_SETTING]: {
+        // 只看域主
+        onlyLookDomainMaster: false,
+        // 黑名单
+        blacklist: [],
+    },
 }
 
 // 获取命名空间内的对象
@@ -25,9 +43,18 @@ export const localStorageInit = () => {
     if (!localStorage.getItem(NAMESPACE)) {
         localStorage.setItem(NAMESPACE, JSON.stringify({}));
     }
+    // 空间内，对于声明的内容进行初始化
     for (let modelMenuKey in ModelMenu) {
         if (!getContent(ModelMenu[modelMenuKey])) {
-            setContent(ModelMenu[modelMenuKey], null);
+            setContent(ModelMenu[modelMenuKey], initContent[modelMenuKey] || null);
+        }
+    }
+    // 空间内，对于没有声明的内容进行销毁
+    for (let key in JSON.parse(localStorage.getItem(NAMESPACE))) {
+        if (!Object.values(ModelMenu).includes(key)) {
+            const storage = JSON.parse(localStorage.getItem(NAMESPACE))
+            delete storage[key]
+            localStorage.setItem(NAMESPACE, JSON.stringify(storage))
         }
     }
 }
